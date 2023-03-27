20f4866334ed228b08a4326db64c6bd093213359
faisalill <mfaisal.ilyas.d@gmail.com>
2023-03-27 22:22:48 +0530

    changed js to css loader

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,13 +1,13 @@
-import { Spin } from 'antd'
 import React from 'react'
 
 const Loading = () => {
   return (
     <>
     <div className='flex justify-center relative top-16'>
-        <Spin tip='Loading' size='large'>
+        {/* <Spin tip='Loading' size='large'>
 
-        </Spin>
+        </Spin> */}      
+        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
     </div>
     </>
   )
